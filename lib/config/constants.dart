421d63492353a93c5957223c62de4b03ad1c1d4e
chius-me <chius.me@outlook.com>
2026-05-22 14:34:18 +0800

    chore: update backend URL to api.globi.lan.tamochi.cn, remove globi-admin reference

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   AppConstants._();
 
-  static const String backendBaseUrl = 'https://server.globi.lan.tamochi.cn';
+  static const String backendBaseUrl = 'https://api.globi.lan.tamochi.cn';
   static const String redirectUri = 'flutty://login-callback';
   static const String redirectScheme = 'flutty';
   static const String redirectHost = 'login-callback';
